2119d82b7fc2be1385a6b33d7bc687a7ca102187
upp <geolifestudy@gmail.com>
2022-03-16 00:36:44 +0800

    添加vuex

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,14 @@
   <!-- 左侧控制栏 -->
   <!-- <SideBar /> -->
   <!-- 默认加载动图 -->
-  
+
   <!-- 底图 z0-->
-  <CesiumGloble style="z-index:1"/>
-  <BabylonDesign style="z-index:0" />
-  
+  <CesiumGloble style="z-index: 1" />
+  <BabylonDesign style="z-index: 0" />
+
   <!-- 左侧控制栏 z1x0-->
-  <LeftControl style="z-index:2"/>
-
-
+  <LeftControl style="z-index: 2" />
+  <TestControl_________TO________DELETE style="z-index: 999999" />
 </template>
 
 <script setup lang="ts">
@@ -24,10 +23,10 @@
 // import SideBar from "./components/CesiumIndex/SideBar.vue";
 // import HelloWorld from "./components/HelloWorld.vue";
 // import RectangleCreatorDemo from "./components/RectangleCreatorDemo.vue";
-import CesiumGloble from './components/CesiumGloble.vue'
-import BabylonDesign from './components/BabylonDesign.vue'
-import LeftControl from './components/LeftControl.vue';
-
+import CesiumGloble from "./components/CesiumGloble.vue";
+import BabylonDesign from "./components/BabylonDesign.vue";
+import LeftControl from "./components/LeftControl.vue";
+import TestControl_________TO________DELETE from "./components/TestControl_________TO________DELETE.vue";
 </script>
 
 <style>
---
+++ src/components/TestControl_________TO________DELETE.vue
@@ -0,0 +1,47 @@
+
+
+<template>
+  <div class="sider" :class="{ siderOpen: isActive }">
+    <button style="left: 500px; width: 550px" @click="isActive = !isActive">
+      globleControl
+    </button>
+  </div>
+</template>
+
+
+
+<style scoped>
+.sider {
+  /* 动画属性 */
+  transition: left 1s ease-in, width 2s, height 2s, background-color 2s,
+    transform 2s;
+
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  width: 500px;
+  height: 100vh;
+  background-color: rgba(255, 30, 0, 0.918);
+}
+.siderOpen {
+  left: -400px;
+  /* width: 100px; */
+  background-color: rgba(0, 153, 255, 0);
+}
+</style>
+
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+// 初始化后延迟1s出现
+const isActive = ref(true);
+// isActive.value = false
+const startCss = () => {
+  setTimeout(() => {
+    isActive.value = false;
+  }, 1000);
+};
+onMounted(startCss);
+
+// const isActive = ref(false)
+</script>
