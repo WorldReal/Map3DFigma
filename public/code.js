9a4c2628f2dced159e4a58605b1b808caf3b1962
wangxu <geolifestudy@gmail.com>
2022-03-18 17:43:21 +0800

    测试window ts全局变量

--- public/code.js
+++ public/code.js
@@ -16,7 +16,61 @@ figma.ui.onmessage = msg => {
     figma.currentPage.selection = nodes;
     figma.viewport.scrollAndZoomIntoView(nodes);
   }
+
+  if (msg.type === 'CanvasToImage') {
+    const nodes = [];
+    for (let i = 0; i < 3; i++) {
+      const rect = figma.createRectangle();
+      rect.x = i * 150;
+      console.log(msg.image)
+      rect.fills = [{ type: 'IMAGE',scaleMode: "FILL",imageHash: msg.image }];
+      figma.currentPage.appendChild(rect);
+      nodes.push(rect);
+    }
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+
+   
+    const selected = figma.currentPage.selection[0]
+    invertImages(selected, msg.image)
+    // msg.image
+
+  }
   // Make sure to close the plugin when you're done. Otherwise the plugin will
   // keep running, which shows the cancel button at the bottom of the screen.
   figma.closePlugin();
 };
+
+
+async function invertImages(node,msgImage) {
+  debugger
+  console.log('invertImages',msgImage)
+  const newFills = []
+  for (const paint of node.fills) {
+    if (paint.type === 'IMAGE') {
+      // Get the (encoded) bytes for this image.
+      // const image = figma.getImageByHash(paint.imageHash)
+      // const bytes = await image.getBytesAsync()
+
+      // // Create an invisible iframe to act as a "worker" which
+      // // will do the task of decoding and send us a message
+      // // when it's done.
+      // figma.showUI(__html__, { visible: false })
+
+      // // Send the raw bytes of the file to the worker.
+      // figma.ui.postMessage(bytes)
+
+      // // Wait for the worker's response.
+      // const newBytes = await new Promise((resolve, reject) => {
+      //   figma.ui.onmessage = value => resolve(value)
+      // })
+
+      // // Create a new paint for the new image.
+      // const newPaint = JSON.parse(JSON.stringify(paint))
+      // newPaint.imageHash = figma.createImage(newBytes).hash
+      newPaint.imageHash = msgImage
+      newFills.push(newPaint)
+    }
+  }
+  node.fills = newFills
+}
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
-  <!-- <img width="50px" alt="Vue logo" src="./assets/logo.png" /> -->
-  <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
-
-  <!-- <RectangleCreatorDemo /> -->
-
-  <!-- <TemplateVue /> -->
-  <!-- 左侧控制栏 -->
-  <!-- <SideBar /> -->
-  <!-- 默认加载动图 -->
 
   <!-- 底图 z0-->
-  <CesiumGloble style="z-index: 1" />
-  <BabylonDesign style="z-index: 0" />
+  <CesiumGloble class="CesiumGloble" :class="{ AllSceneUp: activeIndexScene=='CesiumGloble'}" />
+  <BabylonDesign class="BabylonDesign" :class="{ AllSceneUp: activeIndexScene=='BabylonDesign'}"  />
+  <CBScence class="CBScence" :class="{ AllSceneUp: activeIndexScene=='CBScence'}"/>
+  <!--另外一种写法-->
+  <!-- <CesiumGloble class="CesiumGloble" :class="(activeIndexScene=='CesiumGloble')?'':'AllSceneUp' " /> -->
+  
 
   <!-- 左侧控制栏 z1x0-->
-  <LeftControl style="z-index: 2" />
+  <LeftControl  style="z-index: 10" />
+  <!-- 测试控制栏 -->
   <test_to_delete_control style="z-index: 999999" />
 </template>
 
 <script setup lang="ts">
-// import TemplateVue from "./components/CesiumIndex/Template.vue";
-// import SideBar from "./components/CesiumIndex/SideBar.vue";
-// import HelloWorld from "./components/HelloWorld.vue";
-// import RectangleCreatorDemo from "./components/RectangleCreatorDemo.vue";
+import CBScence from './components/CBScence.vue';
 import CesiumGloble from "./components/CesiumGloble.vue";
 import BabylonDesign from "./components/BabylonDesign.vue";
 import LeftControl from "./components/LeftControl.vue";
 import test_to_delete_control from "./components/test_to_delete_control.vue";
+// 切换视图
+import { computed, ref } from "vue";
+import { useStore } from "vuex";
+const store = useStore();
+// 全局状态选择引擎和场景
+// const activeIndexScene = ref('');
+// activeIndexScene.value = store.state.EngineScene.AllSceneUp;
+const activeIndexScene = computed(() => {
+  return store.state.EngineScene.AllSceneUp;
+});
+
 </script>
 
 <style>
@@ -35,5 +39,25 @@ body {
   margin: 0;
   width: 100vw;
   height: 100vh;
+}
+/* 场景基础层叠切换 
+  CesiumGloble 3
+  CB 12 BabylonDesign0  
+  当前最顶层AllSceneUp5
+  
+  切换状态vuex全局控制 */
+.CesiumGloble{
+z-index: 3
+}
+.BabylonDesign{
+z-index: 2
+}
+.CBScence{/*融合场景*/
+/* TODO Z-index双引擎嵌套问题 */
+z-index: 0
+}
+
+.AllSceneUp{
+  z-index: 5
 }
 </style>
