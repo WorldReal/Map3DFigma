e492aa4f0cac17d524cb773ac7c07ac8bd1041ca
wangxu <geolifestudy@gmail.com>
2022-03-21 18:01:46 +0800

    类型推导  转换像素为png字节流

--- public/code.js
+++ public/code.js
@@ -4,83 +4,19 @@ figma.showUI(__html__, { width: 768, height: 480, title: 'Map3DFigma' });
 figma.ui.onmessage = msg => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
-  }
 
   if (msg.type === 'CanvasToImage') {
     const nodes = [];
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 1; i++) {
       const rect = figma.createRectangle();
       rect.x = i * 150;
-      console.log('image', msg.image)
-      let testimagehash = msg.image.replace('data:image/png;base64,', '')
-      console.log('testimagehash', testimagehash)
-      // rect.fills = [{ type: 'IMAGE',scaleMode: "FILL",imageHash: msg.image. }];
-      rect.fills = [{ type: 'IMAGE', scaleMode: "FILL", imageHash: testimagehash }];
-      // console.log('!!!', msg.newBytes)
-      // console.log('!!!', figma.createImage(msg.newBytes).hash)
-      // rect.fills = [{ type: 'IMAGE', scaleMode: "FILL", imageHash: figma.createImage(msg.newBytes).hash }];
-
+      rect.fills = [{ type: 'IMAGE', scaleMode: "FILL", imageHash: figma.createImage(msg.newBytes).hash }];
       figma.currentPage.appendChild(rect);
       nodes.push(rect);
     }
     figma.currentPage.selection = nodes;
     figma.viewport.scrollAndZoomIntoView(nodes);
 
-    debugger
-    const selected = figma.currentPage.selection[0]
-    // invertImages(selected, msg.image)
-    // msg.image
-
   }
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
+
 };
-
-
-async function invertImages(node, msgImage) {
-  debugger
-  console.log('invertImages', msgImage)
-  const newFills = []
-  for (const paint of node.fills) {
-    if (paint.type === 'IMAGE') {
-      // Get the (encoded) bytes for this image.
-      // const image = figma.getImageByHash(paint.imageHash)
-      // const bytes = await image.getBytesAsync()
-
-      // // Create an invisible iframe to act as a "worker" which
-      // // will do the task of decoding and send us a message
-      // // when it's done.
-      // figma.showUI(__html__, { visible: false })
-
-      // // Send the raw bytes of the file to the worker.
-      // figma.ui.postMessage(bytes)
-
-      // // Wait for the worker's response.
-      // const newBytes = await new Promise((resolve, reject) => {
-      //   figma.ui.onmessage = value => resolve(value)
-      // })
-
-      // // Create a new paint for the new image.
-      // const newPaint = JSON.parse(JSON.stringify(paint))
-      // newPaint.imageHash = figma.createImage(newBytes).hash
-
-
-      // newPaint.imageHash = msgImage
-      newPaint.imageHash = figma.createImage(newBytes).hash
-      newFills.push(newPaint)
-    }
-  }
-  node.fills = newFills
-}
--- src/components/test_to_delete_control/CanvasToImage.vue
+++ src/components/test_to_delete_control/CanvasToImage.vue
@@ -7,26 +7,46 @@
 </template>
 
 <script setup lang='ts'>
-import { reactive, ref, onBeforeMount, onMounted } from "vue";
+import { reactive, ref, onBeforeMount, onMounted } from 'vue'
+// import * as png from "../../assets/js/encode-png.mjs"
 //开始创建组件
-import { useStore } from "vuex";
-const store = useStore();
-let image = ref("aa");
+import { useStore } from 'vuex'
+const store = useStore()
+let image = ref('aa')
 // // 模块A
 // const store_state_ma_count_first = ref(0);
 // store_state_ma_count_first.value = store.state.ma.count;
 
-let CanvasToImage = () => {
+let CanvasToImage = async () => {
   // setTimeout(() => {
   // viewer.render()
-  let viewer = store.state.EngineScene.CesiumGlobleCanvas;
-  let canvasThis = viewer.scene.canvas;
-  image.value = canvasThis.toDataURL("image/png");
-  debugger;
-  let cesiumGl = canvasThis.getContext("webgl");
+  let viewer = store.state.EngineScene.CesiumGlobleCanvas
+  let canvasThis = viewer.scene.canvas
+  image.value = canvasThis.toDataURL('image/png')
+
+
+
+    const newBytes = await new Promise<ArrayBuffer>((resolve, reject) => {
+   canvasThis.toBlob((blob:Blob) => {
+     console.log(blob)
+      const reader = new FileReader()
+      blob&&reader.readAsArrayBuffer(blob)
+      console.log('blob',blob)
+      debugger
+      reader.onload = () => {
+        let result = reader.result as ArrayBuffer
+        resolve(new Uint8Array(result))
+        
+        }
+      reader.onerror = () => reject(new Error('Could not read from blob'))
+    })
+  })
+
+  debugger
+  let cesiumGl = canvasThis.getContext('webgl')
   let pixels = new Uint8Array(
     cesiumGl.drawingBufferWidth * cesiumGl.drawingBufferHeight * 4
-  );
+  )
   cesiumGl.readPixels(
     0,
     0,
@@ -35,21 +55,42 @@ let CanvasToImage = () => {
     cesiumGl.RGBA,
     cesiumGl.UNSIGNED_BYTE,
     pixels
-  );
+  )
+//   const canvasHere = document.createElement('canvas')
+//   const ctxHere = canvasHere.getContext('2d')
+//   canvasHere.width = cesiumGl.drawingBufferWidth
+//   canvasHere.height = cesiumGl.drawingBufferHeightlet
+//   let imageData =new ImageData(new Uint8ClampedArray(pixels), cesiumGl.drawingBufferWidth, cesiumGl.drawingBufferHeightlet)
+// console.log('imageData',imageData)
+//  ctxHere&&ctxHere.putImageData(imageData, 0, 0)
+//  console.log('ctxHere',ctxHere)
+  // const newBytes = await new Promise((resolve, reject) => {
+  //   canvasHere.toBlob((blob) => {
+  //     const reader = new FileReader()
+  //     blob&&reader.readAsArrayBuffer(blob)
 
-  debugger;
+  //     console.log('blob',blob)
+
+  //     debugger
+  //     reader.onload = () => resolve(reader.result)
+  //     reader.onerror = () => reject(new Error('Could not read from blob'))
+  //   })
+  // })
+  console.log('newBytes',newBytes)
+  debugger
   parent.postMessage(
     {
       pluginMessage: {
-        type: "CanvasToImage",
+        type: 'CanvasToImage',
         image: image.value,
-        newBytes: pixels
+        // newBytes: pixels.slice(0,200)
+        newBytes: newBytes
       }
     },
-    "*"
-  );
+    '*'
+  )
   // }, 5000)
-};
+}
 </script>
 <style scoped>
 </style>
